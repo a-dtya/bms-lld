8838001d709c81da92d2e6cd956c8b83c6349e71
admin <adithyavinod1943@gmail.com>
2025-04-14 17:13:46 +0530

    migrated to spring boot

---
+++ src/main/java/ticket/booking/App.java
@@ -0,0 +1,9 @@
+package ticket.booking;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+}
